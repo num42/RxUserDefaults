a542519296984e9e08bb1a56ad2184f058481e61
Wolfgang Lutz <wlut@num42.de>
2018-04-20 17:59:56 +0200

    use xcodegen
    fix readme
    
    do not assume xcodegen is installed
    
    Cleanup
    
    fix travis
    
    cleanup
    
    Simplifications and Improvements
    
    update Travis Schemes
    
    more travis fixes
    
    add caching
    
    try another travis fix
    
    update fastlane dep

--- Gemfile
+++ Gemfile
@@ -4,7 +4,5 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
-
 # Added at 2017-12-08 13:13:18 +0100 by admin:
-gem "fastlane", "~> 2.68"
+gem "fastlane"
---
+++ Headers/RxUserDefaults.h
@@ -0,0 +1,17 @@
+//
+//  RxUserDefaults.h
+//  RxUserDefaults
+//
+//  Created by Wolfgang Lutz on 08.12.17.
+//  Copyright © 2017 Number42 GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxUserDefaults.
+FOUNDATION_EXPORT double RxUserDefaults_VersionNumber;
+
+//! Project version string for RxUserDefaults.
+FOUNDATION_EXPORT const unsigned char RxUserDefaults_VersionString[];
+
+// In this header, you should import all the public headers of your RxUserDefaults using statements like #import <RxUserDefaults/PublicHeader.h>
