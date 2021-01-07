76fe525433c941ec79e92f09fa6f32a1b7aa0318
Wolfgang Lutz <wlut@num42.de>
2021-01-07 14:53:15 +0100

    Remove Carthage Suppport

--- Examples/SetupCarthage.sh
+++
@@ -1,8 +0,0 @@
-# remove local carthage git from Cartfile
-cat Cartfile | grep -v "file" > NewCartfile
-mv NewCartfile Cartfile
-
-# Add Dependency to the parent folder
-echo "git \"file://$(pwd)/../../RxUserDefaults\"" >> Cartfile
-
-carthage bootstrap --no-use-binaries --platform ios
