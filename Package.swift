f1585885fb0fbb2eea9ba5c5c91848264a9b3638
Wolfgang Lutz <WLBORg@gmx.de>
2023-08-02 16:07:58 +0200

    Update and Format project files

--- Package.swift
+++ Package.swift
@@ -2,18 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "RxUserDefaults",
-    products: [
-        .library(name: "RxUserDefaults", targets: ["RxUserDefaults"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-    ],
-    targets: [
-        .target(
-            name: "RxUserDefaults",
-            dependencies: ["RxSwift", "RxCocoa"],
-            path: "Sources"
-        )
-    ]
+  name: "RxUserDefaults",
+  products: [
+    .library(name: "RxUserDefaults", targets: ["RxUserDefaults"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
+  ],
+  targets: [
+    .target(
+      name: "RxUserDefaults",
+      dependencies: ["RxSwift", "RxCocoa"],
+      path: "Sources"
+    )
+  ]
 )
